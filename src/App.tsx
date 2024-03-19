724e375d4a70fd70291e524a2564775ba2853ad2
grace-coding <sanwind0305@gmail.com>
2024-03-19 23:34:46 +0200

    add react project BiTube

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { PageHeader } from "./layouts/PageHeader"
+export default function APP() {
+  return (
+    <div className="max-h-screen flex flex-col">
+      <PageHeader></PageHeader>
+      <div>2</div>
+    </div>
+  )
+}
